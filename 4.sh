b25dc4020761de3e22f0bb8e72fb6364aef3a735
yhc001yhc <103543975+yhc001yhc@users.noreply.github.com>
2024-05-09 18:47:09 +0800

    Create 4.sh

---
+++ 4.sh
@@ -0,0 +1,8 @@
+# 开启cron服务
+service cron start
+
+# 添加每日查询余额的cron作业
+(crontab -l 2>/dev/null; echo "0 0 * * * /root/2.sh") | crontab -
+
+# 添加每3天自动提取余额的cron作业
+(crontab -l 2>/dev/null; echo "0 0 */3 * * /root/3.sh") | crontab -
