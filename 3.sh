6c9cb1e220a199b2bae2a78537000985b4656d9e
yhc001yhc <103543975+yhc001yhc@users.noreply.github.com>
2024-05-09 02:47:52 -0700

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -1,2 +1,16 @@
-chmod +x 2.sh
-./2.sh
+#!/bin/bash
+
+NODE_COUNT=30
+LOG_FILE=query_balance.log
+
+echo "-----------------------" >> $LOG_FILE
+echo "Query Run at $(date)" >> $LOG_FILE
+
+for i in $(seq 1 $NODE_COUNT); do
+    NODE_DIR=$(pwd)/nodes/node-$i
+    PRIV_KEY_FILE=$NODE_DIR/privkey.json
+
+    # 查询余额
+    BALANCE=$(./aioznode reward balance --home $NODE_DIR | jq -r '.balance | select(.!=null) | tonumber')
+    echo "Node $i: Current balance is $BALANCE" >> $LOG_FILE
+done
