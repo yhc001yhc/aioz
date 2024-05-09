957b2e7792549f6d03683178a754234ada7ee5b0
yhc001yhc <103543975+yhc001yhc@users.noreply.github.com>
2024-05-09 19:32:07 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,38 +1,39 @@
 #!/bin/bash
 
-LOG_FILE=withdraw_rewards.log
+LOG_FILE="combined_operations.log"
 NODE_COUNT=20
-BASE_PORT=1317
 WITHDRAW_ADDRESS="0xdfF37251694167f6cb95D6CdB7Fa70cb56eb62C3" # 替换为你的提取地址
+BASE_PORT=1317
 
 echo "-----------------------" >> $LOG_FILE
-echo "Withdrawal Run at $(date)" >> $LOG_FILE
+echo "Operations Run at $(date)" >> $LOG_FILE
 
 for ((i=1; i<=NODE_COUNT; i++)); do
     NODE_DIR=$(pwd)/nodes/node-$i
     PORT=$((BASE_PORT + i))
-    PRIV_KEY_FILE=$NODE_DIR/privkey.json
 
     # 查询余额
     RESPONSE=$(./aioznode reward balance --home $NODE_DIR --endpoint http://127.0.0.1:$PORT)
-    BALANCE=$(echo "$RESPONSE" | jq -r '.balance // "0"')
+    BALANCE=$(echo "$RESPONSE" | jq -r '.balance[] // "0"')
+    WITHDRAW=$(echo "$RESPONSE" | jq -r '.withdraw[] // "0"')
 
-    # 1 aiozcoin = 1000000000000000000 wei
-    # 将余额转换为aioz并四舍五入到最接近的小数点后一位
-    BALANCE_AIOZ=$(echo "scale=1; $BALANCE/1000000000000000000" | bc)
+    # 由于BALANCE和WITHDRAW是以基本单位返回，我们需要将它们转换并计算最终余额
+    FINAL_BALANCE_AIOZ=$(echo "$BALANCE - $WITHDRAW" | bc)
+    FINAL_BALANCE_AIOZ=$(echo "scale=18; $FINAL_BALANCE_AIOZ / 1000000000000000000" | bc)
 
-    if (( $(echo "$BALANCE_AIOZ > 0.05" | bc -l) )); then
-        # 提取余额
-        WITHDRAWAL_AMOUNT=$(echo "$BALANCE_AIOZ/1" | bc) # 整数部分
-        WITHDRAW_RESPONSE=$(./aioznode reward withdraw --address $WITHDRAW_ADDRESS --amount ${WITHDRAWAL_AMOUNT}.0aioz --priv-key-file $PRIV_KEY_FILE --home $NODE_DIR --endpoint http://127.0.0.1:$PORT)
+    echo "Node $i: Calculated final balance is $FINAL_BALANCE_AIOZ AIOZ" >> $LOG_FILE
+    
+    # 检查是否有余额可提取
+    if (( $(echo "$FINAL_BALANCE_AIOZ > 0.0" | bc -l) )); then
+        WITHDRAW_RESPONSE=$(./aioznode reward withdraw --address $WITHDRAW_ADDRESS --amount ${FINAL_BALANCE_AIOZ}aioz --priv-key-file $NODE_DIR/privkey.json --home $NODE_DIR --endpoint http://127.0.0.1:$PORT)
         
         # 验证提取是否成功
         if [[ "$WITHDRAW_RESPONSE" == *"success"* ]]; then
-            echo "Successful withdrawal for node $i: $BALANCE_AIOZ aioz" >> $LOG_FILE
+            echo "Successful: Withdraw of $FINAL_BALANCE_AIOZ AIOZ for node $i" >> $LOG_FILE
         else
-            echo "Withdrawal failed for node $i, received response: $WITHDRAW_RESPONSE" >> $LOG_FILE
+            echo "Failed: Withdraw attempt for node $i, response: $WITHDRAW_RESPONSE" >> $LOG_FILE
         fi
     else
-        echo "Not enough balance to withdraw for node $i, balance is $BALANCE_AIOZ aioz" >> $LOG_FILE
+        echo "No balance to withdraw for node $i after accounting for withdraws." >> $LOG_FILE
     fi
 done
