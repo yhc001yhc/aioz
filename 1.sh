be70801027615733dd281d7218343a3ba6f2f046
yhc001yhc <103543975+yhc001yhc@users.noreply.github.com>
2024-05-09 20:42:41 -0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,39 +1,31 @@
 #!/bin/bash
 
-LOG_FILE="combined_operations.log"
 NODE_COUNT=20
-WITHDRAW_ADDRESS="0xdfF37251694167f6cb95D6CdB7Fa70cb56eb62C3" # 替换为你的提取地址
+WITHDRAW_ADDRESS="0xdfF37251694167f6cb95D6CdB7Fa70cb56eb62C3"
 BASE_PORT=1317
+LOG_FILE="/root/withdraw.log"
 
-echo "-----------------------" >> $LOG_FILE
-echo "Operations Run at $(date)" >> $LOG_FILE
+echo "-----------------------" >> "$LOG_FILE"
+echo "Withdraw Run at $(date)" >> "$LOG_FILE"
 
 for ((i=1; i<=NODE_COUNT; i++)); do
-    NODE_DIR=$(pwd)/nodes/node-$i
+    NODE_DIR="/path/to/nodes/node-$i"
     PORT=$((BASE_PORT + i))
+    ENDPOINT="http://127.0.0.1:$PORT"
+    PRIV_KEY_FILE="$NODE_DIR/privkey.json"
 
-    # 查询余额
-    RESPONSE=$(./aioznode reward balance --home $NODE_DIR --endpoint http://127.0.0.1:$PORT)
-    BALANCE=$(echo "$RESPONSE" | jq -r '.balance[] // "0"')
-    WITHDRAW=$(echo "$RESPONSE" | jq -r '.withdraw[] // "0"')
+    RESPONSE=$(./aioznode reward balance --home "$NODE_DIR" --endpoint "$ENDPOINT")
+    BALANCE=$(echo "$RESPONSE" | jq -r '.balance[0].amount // "0"')
 
-    # 由于BALANCE和WITHDRAW是以基本单位返回，我们需要将它们转换并计算最终余额
-    FINAL_BALANCE_AIOZ=$(echo "$BALANCE - $WITHDRAW" | bc)
-    FINAL_BALANCE_AIOZ=$(echo "scale=18; $FINAL_BALANCE_AIOZ / 1000000000000000000" | bc)
-
-    echo "Node $i: Calculated final balance is $FINAL_BALANCE_AIOZ AIOZ" >> $LOG_FILE
-    
-    # 检查是否有余额可提取
-    if (( $(echo "$FINAL_BALANCE_AIOZ > 0.0" | bc -l) )); then
-        WITHDRAW_RESPONSE=$(./aioznode reward withdraw --address $WITHDRAW_ADDRESS --amount ${FINAL_BALANCE_AIOZ}aioz --priv-key-file $NODE_DIR/privkey.json --home $NODE_DIR --endpoint http://127.0.0.1:$PORT)
-        
-        # 验证提取是否成功
-        if [[ "$WITHDRAW_RESPONSE" == *"success"* ]]; then
-            echo "Successful: Withdraw of $FINAL_BALANCE_AIOZ AIOZ for node $i" >> $LOG_FILE
-        else
-            echo "Failed: Withdraw attempt for node $i, response: $WITHDRAW_RESPONSE" >> $LOG_FILE
-        fi
+    if [ "$BALANCE" -gt 0 ]; then
+        FINAL_BALANCE_AIOZ=$(echo "scale=18; $BALANCE / 1000000000000000000" | bc | awk '{printf "%.18f\n", $0}')
+        WITHDRAW_RESPONSE=$(./aioznode reward withdraw --address "$WITHDRAW_ADDRESS" --amount "${FINAL_BALANCE_AIOZ}aioz" --priv-key-file "$PRIV_KEY_FILE" --home "$NODE_DIR" --endpoint "$ENDPOINT")
+        echo "$WITHDRAW_RESPONSE" >> "$LOG_FILE"
     else
-        echo "No balance to withdraw for node $i after accounting for withdraws." >> $LOG_FILE
+        echo "Node $i: No balance to withdraw." >> "$LOG_FILE"
+    fi
+    
+    if [ $i -lt $NODE_COUNT ]; then
+        sleep 3900
     fi
 done
