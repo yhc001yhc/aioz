c756f5999d8a009c067de4ea05354dcbbe1ec6c4
yhc001yhc <103543975+yhc001yhc@users.noreply.github.com>
2024-05-09 20:45:43 -0700

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -1,8 +1,16 @@
-# 开启cron服务
-service cron start
+#!/bin/bash
 
-# 添加每日查询余额的cron作业
-(crontab -l 2>/dev/null; echo "0 0 * * * /root/3.sh") | crontab -
+# 定义你的提现脚本路径
+SCRIPT_PATH="/root/1.sh"
 
-# 添加每3天自动提取余额的cron作业
-(crontab -l 2>/dev/null; echo "0 0 */3 * * /root/1.sh") | crontab -
+# 定义日志文件路径
+LOG_PATH="/root/withdraw.log"
+
+# 新的Cron作业
+CRON_JOB="0 0 */5 * * nohup $SCRIPT_PATH > $LOG_PATH 2>&1"
+
+# 将新的Cron作业添加到Crontab中，不影响现有的Cron作业
+(crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
+
+# 输出当前用户的Crontab，以便检查是否添加成功
+crontab -l
