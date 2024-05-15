8ac517990ec7e07441da936be54d50c84a8aa883
yhc001yhc <103543975+yhc001yhc@users.noreply.github.com>
2024-05-14 17:26:09 -0700

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -6,11 +6,15 @@ SCRIPT_PATH="/root/1.sh"
 # 定义日志文件路径
 LOG_PATH="/root/withdraw.log"
 
-# 新的Cron作业
-CRON_JOB="0 0 */5 * * nohup $SCRIPT_PATH > $LOG_PATH 2>&1"
+# 每3天运行1.sh脚本的Cron作业
+THREE_DAYS_CRON_JOB="0 0 */3 * * $SCRIPT_PATH > $LOG_PATH 2>&1"
 
-# 将新的Cron作业添加到Crontab中，不影响现有的Cron作业
-(crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
+# 每天运行3.sh脚本的Cron作业
+DAILY_CRON_JOB="0 0 * * * /root/3.sh"
+
+# 使用 grep -v 从 crontab 列表中删除已存在的 1.sh 和 3.sh 相关的cron作业
+# 然后添加新的 cron 作业
+(crontab -l 2>/dev/null | grep -v -E '1.sh|3.sh'; echo "$DAILY_CRON_JOB"; echo "$THREE_DAYS_CRON_JOB") | crontab -
 
 # 输出当前用户的Crontab，以便检查是否添加成功
 crontab -l
