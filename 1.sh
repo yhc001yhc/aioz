fe6ced9ca4d8f1b5f56d1d2523c13b61a828d35e
yhc001yhc <103543975+yhc001yhc@users.noreply.github.com>
2024-05-18 18:34:56 -0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -14,17 +14,22 @@ for ((i=1; i<=NODE_COUNT; i++)); do
     ENDPOINT="http://127.0.0.1:$PORT"
     PRIV_KEY_FILE="$NODE_DIR/privkey.json"
 
+    SCREEN_NAME="withdraw_node_$i"
+
     RESPONSE=$(./aioznode reward balance --home "$NODE_DIR" --endpoint "$ENDPOINT")
     BALANCE=$(echo "$RESPONSE" | jq -r '.balance[0].amount // "0"')
 
     if [ "$BALANCE" -gt 0 ]; then
         FINAL_BALANCE_AIOZ=$(echo "scale=18; $BALANCE / 1000000000000000000" | bc | awk '{printf "%.18f\n", $0}')
-        WITHDRAW_RESPONSE=$(./aioznode reward withdraw --address "$WITHDRAW_ADDRESS" --amount "${FINAL_BALANCE_AIOZ}aioz" --priv-key-file "$PRIV_KEY_FILE" --home "$NODE_DIR" --endpoint "$ENDPOINT")
-        echo "$WITHDRAW_RESPONSE" >> "$LOG_FILE"
+        SCREEN_COMMAND="./aioznode reward withdraw --address '$WITHDRAW_ADDRESS' --amount '${FINAL_BALANCE_AIOZ}aioz' --priv-key-file '$PRIV_KEY_FILE' --home '$NODE_DIR' --endpoint '$ENDPOINT'"
+
+        screen -dmS $SCREEN_NAME bash -c "$SCREEN_COMMAND >> $LOG_FILE"
+
+        echo "Withdraw command for Node $i has been run in screen session $SCREEN_NAME" >> "$LOG_FILE"
     else
         echo "Node $i: No balance to withdraw." >> "$LOG_FILE"
     fi
-    
+
     if [ $i -lt $NODE_COUNT ]; then
         sleep 3900
     fi
