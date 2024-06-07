e49b2645d0b8c36a135e8062ae0e084f37c71d2b
yhc001yhc <103543975+yhc001yhc@users.noreply.github.com>
2024-06-07 02:26:13 -0700

    Update 8.sh

--- 8.sh
+++ 8.sh
@@ -63,7 +63,7 @@ cd /root
 
 
 # 运行带有存储限制的 Docker 容器
-docker run --name station --detach --env FIL_WALLET_ADDRESS=0xea9d5983c9391ec3e40870e7b4c8051756a83a82 ghcr.io/filecoin-station/core
+docker run --name station --detach --env FIL_WALLET_ADDRESS=0x462850374ba447bee3bc6e732868c5e707714356 ghcr.io/filecoin-station/core
 docker run -d --name watchtower --restart=always -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --interval 36000 --cleanup
 
 # 安装并运行traffmonetizer
