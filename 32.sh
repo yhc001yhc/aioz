c3ee5f9ca04d24f94fb44b5ea2991771a176b2bb
yhc001yhc <103543975+yhc001yhc@users.noreply.github.com>
2024-06-28 19:59:49 -0700

    Update and rename 32 to 32.sh

--- 32.sh
+++ 32.sh
@@ -66,7 +66,7 @@ cd /root
 
 
 # 运行带有存储限制的 Docker 容器
-docker run --name station --detach --env FIL_WALLET_ADDRESS=0xea9d5983c9391ec3e40870e7b4c8051756a83a82 ghcr.io/filecoin-station/core
+docker run --name station --detach --env FIL_WALLET_ADDRESS=0x1577c9085c1ecc629afac5aeb70665004c9e7e25 ghcr.io/filecoin-station/core
 docker run -d --name watchtower --restart=always -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --interval 36000 --cleanup
 
 # 安装并运行traffmonetizer
