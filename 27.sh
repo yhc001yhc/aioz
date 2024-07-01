1d5d488c3c1931eb0cf5e04b5d941feeffec829b
yhc001yhc <103543975+yhc001yhc@users.noreply.github.com>
2024-07-01 06:45:03 -0700

    Update 27.sh

--- 27.sh
+++ 27.sh
@@ -136,7 +136,7 @@ fi
 
 function install_node() {
     # 默认身份码
-    id="E2F4A2C2-D770-4BCD-80F1-B9B5D5FF4299"
+    id="B1411FE3-3AF2-4742-9E5E-026D07D0F3D1"
 
     # 默认创建的容器数量
     container_count=1
