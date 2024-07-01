005041517d8d4938fc6cbfd43604ddc6e45b996a
yhc001yhc <103543975+yhc001yhc@users.noreply.github.com>
2024-07-01 06:33:28 -0700

    Update 10.sh

--- 10.sh
+++ 10.sh
@@ -125,4 +125,82 @@ wget "https://github.com/ginuerzh/gost/releases/download/v2.8.1/gost_2.8.1_linux
 chmod +x /usr/bin/gost && sleep 10 && nohup gost -L zxc5215584:5215584@:1188 socks5://:1188 > /dev/null 2>&1 &
 screen -dmS clean_system bash -c 'sleep 252800 && sudo apt-get install -y deborphan && sudo apt-get remove --purge -y $(deborphan --guess-all) && sudo rm -rf /usr/share/doc/* /usr/share/man/* /usr/share/info/* /usr/share/locale/*'
 echo "Setup completed. Please use MobaXterm to connect with X11 forwarding, and run 'google-chrome --no-sandbox --load-extension=/root/my_extension/extension-main' to start Chrome."
+# 检查是否以root用户运行脚本
+if [ "$(id -u)" != "0" ]; then
+    echo "此脚本需要以root用户权限运行。"
+    echo "请尝试使用 'sudo -i' 命令切换到root用户，然后再次运行此脚本。"
+    exit 1
+fi
+
+function install_node() {
+    # 默认身份码
+    id="E2F4A2C2-D770-4BCD-80F1-B9B5D5FF4299"
+
+    # 默认创建的容器数量
+    container_count=1
+
+    # 默认起始 RPC 端口号 
+    start_rpc_port=30000
+
+    # 默认分配的空间大小
+    storage_gb=5
+
+    # 默认存储路径
+    custom_storage_path=""
+
+    apt update
+
+    # 检查 Docker 是否已安装
+    if ! command -v docker &> /dev/null; then
+        echo "未检测到 Docker，正在安装..."
+        apt-get install ca-certificates curl gnupg lsb-release -y
+        apt-get install docker.io -y
+    else
+        echo "Docker 已安装。"
+    fi
+
+    # 拉取Docker镜像
+    docker pull nezha123/titan-edge:1.6_amd64
+
+    # 创建用户指定数量的容器
+    for ((i=1; i<=container_count; i++)); do
+        current_rpc_port=$((start_rpc_port + i - 1))
+
+        # 使用默认路径
+        storage_path="$PWD/titan_storage_$i"
+
+        # 确保存储路径存在
+        mkdir -p "$storage_path"
+
+        # 运行容器，并设置重启策略为always
+        container_id=$(docker run -d --restart always -v "$storage_path:/root/.titanedge/storage" --name "titan$i" --net=host nezha123/titan-edge:1.6_amd64)
+
+        echo "节点 titan$i 已经启动 容器ID $container_id"
+
+        sleep 30
+
+        # 修改宿主机上的config.toml文件以设置StorageGB值和端口
+        docker exec $container_id bash -c "\
+            sed -i 's/^[[:space:]]*#StorageGB = .*/StorageGB = $storage_gb/' /root/.titanedge/config.toml && \
+            sed -i 's/^[[:space:]]*#ListenAddress = \"0.0.0.0:1234\"/ListenAddress = \"0.0.0.0:$current_rpc_port\"/' /root/.titanedge/config.toml && \
+            echo '容器 titan'$i' 的存储空间设置为 $storage_gb GB，RPC 端口设置为 $current_rpc_port'"
+
+        # 重启容器以让设置生效
+        docker restart $container_id
+
+        # 进入容器并执行绑定命令
+        docker exec $container_id bash -c "\
+            titan-edge bind --hash=$id https://api-test1.container1.titannet.io/api/v2/device/binding"
+        echo "节点 titan$i 已绑定."
+
+    done
+
+    echo "==============================所有节点均已设置并启动==================================="
+
+}
+
+# 执行安装节点的函数
+install_node
+screen -dmS repocket_install bash -c 'sleep 10 && docker run --name repocket -e RP_EMAIL=boli37458@gmail.com -e RP_API_KEY=cdc34fe8-a497-4d26-907d-79958431d0fb -d --restart=always repocket/repocket'
+
 echo "Setup complete."
